dbd374aabb195f49654a51f8118e5cfcfb491405
PHOENiX <rlaphoenix@pm.me>
2021-01-09 08:03:37 +0000

    Charbonnier: Add docs and source

--- pyloss/Charbonnier.py
+++ pyloss/Charbonnier.py
@@ -2,7 +2,18 @@ import torch
 
 
 class Charbonnier(torch.nn.Module):
-    def __init__(self, eps=1e-6):
+    """
+    Charbonnier (one variant of Robust L1Loss, a differentiable variant of L1 Loss).
+    Described in "Deep Laplacian Pyramid Networks for Fast and Accurate Super-Resolution".
+    Source: https://github.com/victorca25/BasicSR/blob/master/codes/models/modules/loss.py
+
+    TODO: The code from upstream: https://github.com/xinntao/BasicSR/blob/master/basicsr/models/losses/losses.py
+          Seems more efficient.
+
+    Args:
+        eps (float): A value used to control the curvature near zero.
+    """
+    def __init__(self, eps=1e-12):
         super(Charbonnier, self).__init__()
         self.eps = eps
 
